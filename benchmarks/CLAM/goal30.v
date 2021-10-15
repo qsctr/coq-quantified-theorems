7526608084595e4395b0d33d3e697c64fa088c56
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2021-10-15 13:16:19 -0400

    Add solution to CLAM/goal30

--- benchmarks/CLAM/goal30.v
+++ benchmarks/CLAM/goal30.v
@@ -21,7 +21,56 @@ Fixpoint rev (rev_arg0 : Lst) : Lst
               | cons x y => append (rev y) (cons x nil)
               end.
 
+Theorem append_nil: forall (l: Lst), append l nil = l.
+Proof.
+  induction l.
+  { simpl. f_equal. assumption. }
+  { simpl. reflexivity. }
+Qed.
+
+Theorem append_assoc:
+  forall (l1 l2 l3: Lst), append l1 (append l2 l3) = append (append l1 l2) l3.
+Proof.
+  induction l1; induction l2; induction l3; try (simpl; reflexivity).
+  { simpl. rewrite <- IHl1. f_equal. }
+  { simpl. rewrite 2 append_nil. reflexivity. }
+  { simpl. rewrite append_nil.  reflexivity. }
+  { simpl. rewrite 2 append_nil. reflexivity. }
+Qed.
+
+Theorem append_rev_cons:
+  forall (l1 l2: Lst) (x: Nat),
+    rev (append l1 (cons x l2)) = append (rev l2) (cons x (rev l1)).
+Proof.
+  induction l1; induction l2; try (simpl; reflexivity).
+  { intro. simpl. rewrite IHl1. simpl. rewrite <- append_assoc.
+    f_equal. }
+  { intro. simpl. rewrite IHl1. simpl. reflexivity. }
+Qed.
+
+Theorem rev_append: forall (l1 l2: Lst), rev (append l1 l2) = append (rev l2) (rev l1).
+Proof.
+  induction l1.
+  { induction l2.
+    { simpl. rewrite append_rev_cons.
+      rewrite <- 2 append_assoc.
+      f_equal. }
+    { simpl. rewrite append_nil. reflexivity. }
+  }
+  { intro. simpl. rewrite append_nil. reflexivity. }
+Qed.
+
+Theorem rev_involutive : forall (x : Lst), eq (rev (rev x)) x.
+Proof.
+  induction x.
+  { simpl. rewrite rev_append. simpl. f_equal.
+    assumption. }
+  { simpl. reflexivity. }
+Qed.
+
 Theorem theorem0 : forall (x : Lst), eq (rev (append (rev x) nil)) x.
 Proof.
-Admitted.
-
+  intro.
+  rewrite append_nil.
+  apply rev_involutive.
+Qed.
