da1cb5668fe234df251ce1dd15707e2a9803e558
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2021-10-15 13:14:58 -0400

    Add solution to CLAM/goal29

--- benchmarks/CLAM/goal29.v
+++ benchmarks/CLAM/goal29.v
@@ -27,7 +27,75 @@ Fixpoint qreva (qreva_arg0 : Lst) (qreva_arg1 : Lst) : Lst
               | cons z x, y => qreva x (cons z y)
               end.
 
+Theorem append_nil: forall (l: Lst), append l nil = l.
+Proof.
+  induction l.
+  { simpl. f_equal. assumption. }
+  { simpl. reflexivity. }
+Qed.
+
+Theorem append_assoc:
+  forall (l1 l2 l3: Lst), append l1 (append l2 l3) = append (append l1 l2) l3.
+Proof.
+  induction l1; induction l2; induction l3; try (simpl; reflexivity).
+  { simpl. rewrite <- IHl1. f_equal. }
+  { simpl. rewrite 2 append_nil. reflexivity. }
+  { simpl. rewrite append_nil.  reflexivity. }
+  { simpl. rewrite 2 append_nil. reflexivity. }
+Qed.
+
+Theorem append_rev_cons:
+  forall (l1 l2: Lst) (x: Nat),
+    rev (append l1 (cons x l2)) = append (rev l2) (cons x (rev l1)).
+Proof.
+  induction l1; induction l2; try (simpl; reflexivity).
+  { intro. simpl. rewrite IHl1. simpl. rewrite <- append_assoc.
+    f_equal. }
+  { intro. simpl. rewrite IHl1. simpl. reflexivity. }
+Qed.
+
+Theorem rev_append: forall (l1 l2: Lst), rev (append l1 l2) = append (rev l2) (rev l1).
+Proof.
+  induction l1.
+  { induction l2.
+    { simpl. rewrite append_rev_cons.
+      rewrite <- 2 append_assoc.
+      f_equal. }
+    { simpl. rewrite append_nil. reflexivity. }
+  }
+  { intro. simpl. rewrite append_nil. reflexivity. }
+Qed.
+
+Theorem rev_involutive : forall (x : Lst), eq (rev (rev x)) x.
+Proof.
+  induction x.
+  { simpl. rewrite rev_append. simpl. f_equal.
+    assumption. }
+  { simpl. reflexivity. }
+Qed.
+
+Theorem qreva_append : forall (x y : Lst), (qreva x y) = (append (rev x) y).
+Proof.
+  induction x; induction y; simpl; try reflexivity.
+  { rewrite IHx.
+    rewrite <- append_assoc.
+    f_equal. }
+  { rewrite IHx.
+    rewrite append_nil.
+    reflexivity. }
+Qed.
+
+Theorem rev_qreva_nil : forall (x : Lst), eq (rev x) (qreva x nil).
+Proof.
+  intros.
+  rewrite qreva_append.
+  rewrite append_nil.
+  reflexivity.
+Qed.
+
 Theorem theorem0 : forall (x : Lst), eq (rev (qreva x nil)) x.
 Proof.
-Admitted.
-
+  intro.
+  rewrite <- rev_qreva_nil.
+  apply rev_involutive.
+Qed.
