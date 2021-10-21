a74216fd9a769dd425b605a8e50d4ebca6e04734
Bretton <qsctr@users.noreply.github.com>
2021-10-21 02:12:11 -0700

    Prove CLAM/goal43

--- benchmarks/CLAM/goal43.v
+++ benchmarks/CLAM/goal43.v
@@ -1,8 +1,10 @@
-Require Import Nat Arith.
+Require Import Nat Arith Bool.
 
 Inductive Nat : Type := succ : Nat -> Nat |  zero : Nat.
 
-Inductive Lst : Type := cons : Nat -> Lst -> Lst |  nil : Lst.
+Scheme Equality for Nat.
+
+Inductive Lst : Type := nil : Lst | cons : Nat -> Lst -> Lst.
 
 Inductive Tree : Type := node : Nat -> Tree -> Tree -> Tree |  leaf : Tree.
 
@@ -12,13 +14,10 @@ with ZLst : Type := zcons : Pair -> ZLst -> ZLst |  znil : ZLst.
 Fixpoint mem (mem_arg0 : Nat) (mem_arg1 : Lst) : bool
            := match mem_arg0, mem_arg1 with
               | x, nil => false
-              | x, cons y z => orb (eqb x y) (mem x z)
+              | x, cons y z => orb (Nat_beq x y) (mem x z)
               end.
 
-Fixpoint lst_mem (lst_mem_arg0 : Nat) (lst_mem_arg1 : Lst) : bool
-           := match lst_mem_arg0, lst_mem_arg1 with
-              | n, x => mem n x
-              end.
+Definition lst_mem := mem.
 
 Fixpoint lst_union (lst_union_arg0 : Lst) (lst_union_arg1 : Lst) : Lst
            := match lst_union_arg0, lst_union_arg1 with
@@ -28,5 +27,11 @@ Fixpoint lst_union (lst_union_arg0 : Lst) (lst_union_arg1 : Lst) : Lst
 
 Theorem theorem0 : forall (x : Nat) (y : Lst) (z : Lst), eq (lst_mem x y) true -> eq (lst_mem x (lst_union z y)) true.
 Proof.
-Admitted.
+  intros.
+  induction z.
+  - assumption.
+  - simpl. destruct (lst_mem n y).
+    + assumption.
+    + simpl. rewrite IHz. apply orb_true_r.
+Qed.
 
