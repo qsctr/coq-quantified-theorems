e34cf8a847274b273f22445c3d275b58a12ef13e
Bretton <qsctr@users.noreply.github.com>
2021-10-23 16:16:44 -0700

    Prove CLAM/goal62

--- benchmarks/CLAM/goal62.v
+++ benchmarks/CLAM/goal62.v
@@ -1,8 +1,10 @@
-Require Import Nat Arith.
+Require Import Nat Arith Bool.
 
-Inductive Nat : Type := succ : Nat -> Nat |  zero : Nat.
+Inductive Nat : Type := zero : Nat | succ : Nat -> Nat.
 
-Inductive Lst : Type := cons : Nat -> Lst -> Lst |  nil : Lst.
+Scheme Equality for Nat.
+
+Inductive Lst : Type := nil : Lst | cons : Nat -> Lst -> Lst.
 
 Inductive Tree : Type := node : Nat -> Tree -> Tree -> Tree |  leaf : Tree.
 
@@ -16,6 +18,9 @@ Fixpoint less (less_arg0 : Nat) (less_arg1 : Nat) : bool
               | succ x, succ y => less x y
               end.
 
+Definition leq (x : Nat) (y : Nat) : bool :=
+  Nat_beq x y || less x y.
+
 Fixpoint insort (insort_arg0 : Nat) (insort_arg1 : Lst) : Lst
            := match insort_arg0, insort_arg1 with
               | i, nil => cons i nil
@@ -25,8 +30,10 @@ Fixpoint insort (insort_arg0 : Nat) (insort_arg1 : Lst) : Lst
 Fixpoint sorted (sorted_arg0 : Lst) : bool
            := match sorted_arg0 with
               | nil => true
-              | cons x nil => true
-              | cons x (cons z y) => andb (sorted (cons z y)) (leq x z)
+              | cons x l => match l with
+                | nil => true
+                | cons z y => andb (sorted l) (leq x z)
+                end
               end.
 
 Fixpoint sort (sort_arg0 : Lst) : Lst
@@ -35,7 +42,31 @@ Fixpoint sort (sort_arg0 : Lst) : Lst
               | cons x y => insort x (sort y)
               end.
 
-Theorem theorem0 : forall (x : Lst) (y : Nat), eq (sorted x) true -> eq (sorted (insort y x)) true.
+Lemma not_less : forall (x y : Nat), less x y = false -> leq y x = true.
 Proof.
-Admitted.
+  intros.
+  generalize dependent y.
+  induction x.
+  - intros. unfold leq. destruct y.
+    + reflexivity.
+    + discriminate.
+  - intros. unfold leq. destruct y.
+    + reflexivity.
+    + simpl in H. apply IHx in H. unfold leq in H. simpl. assumption.
+Qed.
 
+Theorem theorem0 : forall (x : Lst) (y : Nat), sorted x = true -> sorted (insort y x) = true.
+Proof.
+  intros.
+  induction x.
+  - reflexivity.
+  - destruct x.
+    + simpl. destruct (less y n) eqn:?.
+      * simpl. unfold leq. rewrite Heqb. apply orb_true_r.
+      * simpl. apply not_less. assumption.
+    + simpl in H. apply andb_true_iff in H. destruct H. simpl in IHx. simpl. destruct (less y n) eqn:?.
+      * simpl. rewrite H. rewrite H0. unfold leq. rewrite Heqb. apply orb_true_r.
+      * destruct (less y n0) eqn:?.
+        -- simpl. rewrite H. apply not_less in Heqb. rewrite Heqb. unfold leq. rewrite Heqb0. rewrite orb_true_r. reflexivity.
+        -- simpl. apply IHx in H. simpl in H. rewrite H. rewrite H0. reflexivity.
+Qed.
