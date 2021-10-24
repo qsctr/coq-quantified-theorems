5c0b9cd1fa6d6646d863277acd99c4c55f22eee8
Bretton <qsctr@users.noreply.github.com>
2021-10-23 19:19:48 -0700

    Prove CLAM/goal73

--- benchmarks/CLAM/goal73.v
+++ benchmarks/CLAM/goal73.v
@@ -1,6 +1,6 @@
 Require Import Nat Arith.
 
-Inductive Nat : Type := succ : Nat -> Nat |  zero : Nat.
+Inductive Nat : Type := zero : Nat | succ : Nat -> Nat.
 
 Inductive Lst : Type := cons : Nat -> Lst -> Lst |  nil : Lst.
 
@@ -21,7 +21,51 @@ Fixpoint mult (mult_arg0 : Nat) (mult_arg1 : Nat) : Nat
               | succ n, m => plus (mult n m) m
               end.
 
-Theorem theorem0 : forall (x : Nat) (y : Nat) (z : Nat), eq (mult (mult x y) z) (mult x (mult y z)).
+Lemma plus_succ : forall (x y : Nat), plus x (succ y) = succ (plus x y).
 Proof.
-Admitted.
+  intros.
+  induction x.
+  - reflexivity.
+  - simpl. rewrite IHx. reflexivity.
+Qed.
+
+Lemma plus_assoc : forall (x y z : Nat), plus (plus x y) z = plus x (plus y z).
+Proof.
+  intros.
+  induction x.
+  - reflexivity.
+  - simpl. rewrite IHx. reflexivity.
+Qed.
+
+Lemma plus_zero : forall (x : Nat), plus x zero = x.
+Proof.
+  intros.
+  induction x.
+  - reflexivity.
+  - simpl. rewrite IHx. reflexivity.
+Qed.
+
+Lemma plus_commut : forall (x y : Nat), plus x y = plus y x.
+Proof.
+  intros.
+  induction x.
+  - rewrite plus_zero. reflexivity.
+  - simpl. rewrite plus_succ. rewrite IHx. reflexivity.
+Qed.
+
+Lemma distrib : forall (x y z : Nat), mult (plus x y) z = plus (mult x z) (mult y z).
+Proof.
+  intros.
+  induction x.
+  - reflexivity.
+  - simpl. rewrite IHx. rewrite plus_assoc. rewrite (plus_commut (mult y z) z). rewrite <- plus_assoc. reflexivity.
+Qed.
+
+Theorem mult_assoc : forall (x : Nat) (y : Nat) (z : Nat), eq (mult (mult x y) z) (mult x (mult y z)).
+Proof.
+  intros.
+  induction x.
+  - reflexivity.
+  - simpl. rewrite distrib. rewrite IHx. reflexivity.
+Qed.
 
