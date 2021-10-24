72d3c1586804ebef7355f834957febec0133244d
Bretton <qsctr@users.noreply.github.com>
2021-10-23 19:25:13 -0700

    Prove CLAM/goal75

--- benchmarks/CLAM/goal75.v
+++ benchmarks/CLAM/goal75.v
@@ -2,7 +2,7 @@ Require Import Nat Arith.
 
 Inductive Nat : Type := succ : Nat -> Nat |  zero : Nat.
 
-Inductive Lst : Type := cons : Nat -> Lst -> Lst |  nil : Lst.
+Inductive Lst : Type := nil : Lst | cons : Nat -> Lst -> Lst.
 
 Inductive Tree : Type := node : Nat -> Tree -> Tree -> Tree |  leaf : Tree.
 
@@ -27,7 +27,18 @@ Fixpoint qreva (qreva_arg0 : Lst) (qreva_arg1 : Lst) : Lst
               | cons z x, y => qreva x (cons z y)
               end.
 
+Lemma append_assoc : forall (x y z : Lst), append (append x y) z = append x (append y z).
+Proof.
+  intros.
+  induction x.
+  - reflexivity.
+  - simpl. rewrite IHx. reflexivity.
+Qed.
+
 Theorem theorem0 : forall (x : Lst) (y : Lst), eq (append (rev x) y) (qreva x y).
 Proof.
-Admitted.
+  induction x.
+  - reflexivity.
+  - intros. simpl. rewrite append_assoc. simpl. rewrite IHx. reflexivity.
+Qed.
 
