b47498844279b07ff397bb2f30c4bec05990b1ea
Bretton <qsctr@users.noreply.github.com>
2021-10-23 19:14:21 -0700

    Prove CLAM/goal71

--- benchmarks/CLAM/goal71.v
+++ benchmarks/CLAM/goal71.v
@@ -1,8 +1,10 @@
 Require Import Nat Arith.
 
-Inductive Nat : Type := succ : Nat -> Nat |  zero : Nat.
+Inductive Nat : Type := zero : Nat | succ : Nat -> Nat.
 
-Inductive Lst : Type := cons : Nat -> Lst -> Lst |  nil : Lst.
+Scheme Equality for Nat.
+
+Inductive Lst : Type := nil : Lst | cons : Nat -> Lst -> Lst.
 
 Inductive Tree : Type := node : Nat -> Tree -> Tree -> Tree |  leaf : Tree.
 
@@ -19,7 +21,7 @@ Fixpoint less (less_arg0 : Nat) (less_arg1 : Nat) : bool
 Fixpoint count (count_arg0 : Nat) (count_arg1 : Lst) : Nat
            := match count_arg0, count_arg1 with
               | x, nil => zero
-              | x, cons y z => if eqb x y then succ (count x z) else count x z
+              | x, cons y z => if Nat_beq x y then succ (count x z) else count x z
               end.
 
 Fixpoint insort (insort_arg0 : Nat) (insort_arg1 : Lst) : Lst
@@ -34,7 +36,36 @@ Fixpoint sort (sort_arg0 : Lst) : Lst
               | cons x y => insort x (sort y)
               end.
 
+Lemma Nat_beq_eq : forall (x y : Nat), Nat_beq x y = true -> x = y.
+Proof.
+  intros.
+  generalize dependent y.
+  induction x.
+  - intros. destruct y.
+    + reflexivity.
+    + discriminate.
+  - intros. destruct y.
+    + discriminate.
+    + simpl in H. apply IHx in H. rewrite H. reflexivity.
+Qed.
+
 Theorem theorem0 : forall (x : Nat) (y : Nat) (z : Lst), not (eq x y) -> eq (count x (insort y z)) (count x z).
 Proof.
-Admitted.
+  intros.
+  induction z.
+  - simpl. destruct (Nat_beq x y) eqn:?.
+    + apply Nat_beq_eq in Heqb. contradiction.
+    + reflexivity.
+  - simpl. destruct (Nat_beq x n) eqn:?.
+    + destruct (less y n) eqn:?.
+      * simpl. destruct (Nat_beq x y) eqn:?.
+        -- apply Nat_beq_eq in Heqb1. contradiction.
+        -- rewrite Heqb. reflexivity.
+      * simpl. rewrite Heqb. rewrite IHz. reflexivity.
+    + destruct (less y n) eqn:?.
+      * simpl. destruct (Nat_beq x y) eqn:?.
+        -- apply Nat_beq_eq in Heqb1. contradiction.
+        -- rewrite Heqb. reflexivity.
+      * simpl. rewrite Heqb. assumption.
+Qed.
 
