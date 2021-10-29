fe096ad1bcb12639b1dd360abb798ef3f5fbf0e7
Bretton <qsctr@users.noreply.github.com>
2021-10-29 16:28:28 -0700

    Prove queue_amort

--- benchmarks/queue_amort.v
+++ benchmarks/queue_amort.v
@@ -1,6 +1,6 @@
 Require Import Nat Arith.
 
-Inductive Lst : Type := cons : nat -> Lst -> Lst |  nil : Lst.
+Inductive Lst : Type := nil : Lst | cons : nat -> Lst -> Lst.
 
 Inductive Queue : Type := queue : Lst -> Lst -> Queue.
 
@@ -22,20 +22,22 @@ Fixpoint rev2 (rev2_arg0 : Lst) (rev2_arg1 : Lst) : Lst
               | cons x t, a => rev2 t (cons x a)
               end.
 
-Fixpoint qrev (qrev_arg0 : Lst) : Lst
-           := let 'x := qrev_arg0 in
-              rev2 x nil.
+Definition qrev (x : Lst) : Lst
+  := rev2 x nil.
 
-Fixpoint amortizeQueue (amortizeQueue_arg0 : Lst) (amortizeQueue_arg1 : Lst) : Queue
-           := match amortizeQueue_arg0, amortizeQueue_arg1 with
-              | x, y => if leb (len y) (len x) then queue x y else queue (append x (qrev y)) nil
-              end.
+Definition amortizeQueue (x : Lst) (y : Lst) : Queue
+  := if leb (len y) (len x) then queue x y else queue (append x (qrev y)) nil.
 
-Fixpoint isAmortized (isAmortized_arg0 : Queue) : bool
+Definition isAmortized (isAmortized_arg0 : Queue) : bool
            := let 'queue x y := isAmortized_arg0 in
               leb (len y) (len x).
 
 Theorem theorem0 : forall (x : Lst) (y : Lst), eq (isAmortized (amortizeQueue x y)) true.
 Proof.
-Admitted.
+  intros.
+  unfold amortizeQueue.
+  destruct (len y <=? len x) eqn:?.
+  - simpl. assumption.
+  - simpl. reflexivity.
+Qed.
 
