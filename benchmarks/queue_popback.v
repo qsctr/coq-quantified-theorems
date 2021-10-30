5ff0bc434772d923c49510273f633296b3e90bc4
Bretton <qsctr@users.noreply.github.com>
2021-10-29 22:03:08 -0700

    Prove queue_popback

--- benchmarks/queue_popback.v
+++ benchmarks/queue_popback.v
@@ -1,6 +1,8 @@
-Require Import Nat Arith.
+Require Import Bool Nat Arith.
 
-Inductive Lst : Type := cons : nat -> Lst -> Lst |  nil : Lst.
+Inductive Lst : Type := nil : Lst | cons : nat -> Lst -> Lst.
+
+Scheme Equality for Lst.
 
 Inductive Queue : Type := queue : Lst -> Lst -> Queue.
 
@@ -10,7 +12,7 @@ Fixpoint len (len_arg0 : Lst) : nat
               | cons x y => plus 1 (len y)
               end.
 
-Fixpoint qlen (qlen_arg0 : Queue) : nat
+Definition qlen (qlen_arg0 : Queue) : nat
            := let 'queue x y := qlen_arg0 in
               plus (len x) (len y).
 
@@ -23,24 +25,40 @@ Fixpoint append (append_arg0 : Lst) (append_arg1 : Lst) : Lst
 Fixpoint butlast (butlast_arg0 : Lst) : Lst
            := match butlast_arg0 with
               | nil => nil
-              | cons n x => if eqb x nil then nil else cons n (butlast x)
+              | cons n x => if Lst_beq x nil then nil else cons n (butlast x)
               end.
 
-Fixpoint qpopback (qpopback_arg0 : Queue) : Queue
+Definition qpopback (qpopback_arg0 : Queue) : Queue
            := match qpopback_arg0 with
               | queue x (cons n y) => queue x y
               | queue x nil => queue (butlast x) nil
               end.
 
-Fixpoint isAmortized (isAmortized_arg0 : Queue) : bool
+Definition isAmortized (isAmortized_arg0 : Queue) : bool
            := let 'queue x y := isAmortized_arg0 in
               leb (len y) (len x).
 
-Fixpoint isEmpty (isEmpty_arg0 : Queue) : bool
+Definition isEmpty (isEmpty_arg0 : Queue) : bool
            := let 'queue x y := isEmpty_arg0 in
-              andb (eqb x nil) (eqb y nil).
+              andb (Lst_beq x nil) (Lst_beq y nil).
 
-Theorem theorem0 : forall (q : Queue) (n : nat), and (eq (isAmortized q) true) (not (eq (isEmpty q) true)) -> eq (plus 1 (qlen (qpopback q))) (qlen q).
+Lemma len_butlast : forall (l : Lst) (n : nat), S (len (butlast (cons n l))) = len (cons n l).
 Proof.
-Admitted.
+  intros.
+  generalize dependent n.
+  induction l.
+  - reflexivity.
+  - intros. simpl. simpl in IHl. rewrite IHl. reflexivity.
+Qed.
+
+Theorem theorem0 : forall (q : Queue) (n : nat), isAmortized q && negb (isEmpty q) = true -> eq (plus 1 (qlen (qpopback q))) (qlen q).
+Proof.
+  intros.
+  destruct q.
+  destruct l0.
+  - simpl. rewrite <- plus_n_O. destruct l.
+    + simpl in H. discriminate.
+    + rewrite len_butlast. apply plus_n_O.
+  - simpl. apply plus_n_Sm.
+Qed.
 
