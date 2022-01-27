19327d1a7d87d008af320377acb25a4f458cdfa3
Todd Millstein <todd@cs.ucla.edu>
2022-01-26 18:57:28 -0800

    new inequality benchmark

---
+++ benchmarks/inequalities/len_rev_lt/len_rev_lt.v
@@ -0,0 +1,50 @@
+(* From lfind Require Import LFind. *)
+Unset Printing Notations.
+Set Printing Implicit.
+Require Import Nat Arith.
+
+Fixpoint plus (n1 : nat) (n2 : nat) : nat :=
+  match n1 with
+  | O => n2
+  | S n1' => S (plus n1' n2)
+  end.
+
+Inductive lst : Type :=
+  | Nil : lst
+  | Cons : nat -> lst -> lst.
+
+Fixpoint len (l : lst) : nat :=
+  match l with
+  | Nil => 0
+  | Cons x y => plus 1 (len y)
+  end.
+
+Fixpoint append (l1 : lst) (l2 : lst) : lst :=
+  match l1 with
+  | Nil => l2
+  | Cons x y => Cons x (append y l2)
+  end.
+
+Fixpoint rev (l : lst) : lst :=
+  match l with
+  | Nil => Nil
+  | Cons x y => append (rev y) (Cons x Nil)
+  end.
+
+Lemma len_append : forall x y : lst, len (append x y) < S (plus (len x) (len y)).
+Proof.
+  intros.
+  induction x.
+  - simpl. apply le_refl.
+  - simpl. apply le_n_S. assumption.
+Qed.
+
+
+
+Theorem len_rev : forall x : lst, len (rev x) < S (len x).
+Proof.
+  intros.
+  induction x.
+  - simpl. apply le_refl.
+  - simpl. lfind.
+Admitted.
